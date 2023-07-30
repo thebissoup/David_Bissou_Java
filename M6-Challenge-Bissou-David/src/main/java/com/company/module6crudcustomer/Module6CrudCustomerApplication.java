fe94b57a28691fa896974f6070d1e1cebe0d4a6b
DAVID BISSOU <davidebissou@gmail.com>
2023-07-29 23:10:01 -0500

    Completed Module 6

---
+++ M6-Challenge-Bissou-David/src/main/java/com/company/module6crudcustomer/Module6CrudCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.company.module6crudcustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module6CrudCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module6CrudCustomerApplication.class, args);
+	}
+
+}
---
+++ M6-Challenge-Bissou-David/src/main/java/com/company/module6crudcustomer/controller/CustomerController.java
@@ -0,0 +1,56 @@
+package com.company.module6crudcustomer.controller;
+
+import com.company.module6crudcustomer.model.Customer;
+import com.company.module6crudcustomer.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepo;
+
+    //Create a new customer record
+    @PostMapping("/customers")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer addCustomer(@RequestBody Customer customer) { return customerRepo.save(customer); }
+
+    //Update an existing customer record
+    @PutMapping("/customers")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCustomer(@RequestBody Customer customer) { customerRepo.save(customer); }
+
+    //Delete an existing customer record
+    @DeleteMapping("/customers/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable int id) {
+        customerRepo.deleteById(id);
+    }
+
+    //Find a customer record by id
+    @GetMapping("/customers/{id}")
+    public Customer getCustomerById(@PathVariable int id) {
+        Optional<Customer> result = customerRepo.findById(id);
+        if(result.isPresent()) {
+            return result.get();
+        }   else {
+            return null;
+        }
+    }
+
+    //Find customer records by state
+
+    @GetMapping("/customers/state/{state}")
+    public List<Customer> getCustomersByState(@PathVariable String state) {
+        return customerRepo.findCustomerByState(state);
+    }
+
+
+
+
+}
