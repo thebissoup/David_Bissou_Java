0be7e12b1897d59dbecbac92ea2f601b15042ba8
DAVID BISSOU <davidebissou@gmail.com>
2023-07-29 20:27:23 -0500

    Added semi-colons

--- M5-Challenge-Bissou-David/northwind-queries-david-bissou.sql
+++ M5-Challenge-Bissou-David/northwind-queries-david-bissou.sql
@@ -8,48 +8,48 @@ FROM products
 SELECT product_name
 FROM products p 
 WHERE product_name
-LIKE '%Dell%'
+LIKE '%Dell%';
 
 /*List all the orders shipped to Pennsylvania.*/
 
 SELECT *
 FROM orders
-WHERE ship_state = 'Pennsylvania'
+WHERE ship_state = 'Pennsylvania';
 
 /*List the first name and last name of all employees with last names that start with the letter W.*/
 
 SELECT first_name, last_name
 FROM employees
 WHERE last_name
-LIKE 'W%'
+LIKE 'W%';
 
 /*List all customers from zip codes that start with 55.*/
 
 SELECT *
 FROM customers
 WHERE postal_code
-LIKE '55%'
+LIKE '55%';
 
 /*List all customers from zip codes that end with 0.*/
 
 SELECT *
 FROM customers
 WHERE postal_code
-LIKE '%0'
+LIKE '%0';
 
 /*List the first name, last name, and email for all customers with a ".org" email address.*/
 
 SELECT first_name, last_name, email
 FROM customers
 WHERE email
-LIKE '%.org'
+LIKE '%.org';
 
 /*List the first name, last name, and phone number for all customers from the 202 area code.*/
 
 SELECT first_name, last_name, phone
 FROM customers
 WHERE phone
-LIKE '%(202)%'
+LIKE '%(202)%';
 
 /*List the first name, last name, and phone number for all customers from the 202 area code, ordered by last name, first name.*/
 
@@ -57,4 +57,4 @@ SELECT first_name, last_name, phone
 FROM customers
 WHERE phone
 LIKE '%(202)%'
-ORDER BY last_name asc, first_name asc
+ORDER BY last_name asc, first_name asc;
